4a785fbc015b898abe8b87df16cd0bc8ef7a6650
Erica Wood <ericacwood@gmail.com>
2022-07-08 10:47:07 -0700

    #11 tooling to help with this

---
+++ tools/xml_to_json.py
@@ -0,0 +1,19 @@
+import xmltodict
+import json
+import argparse
+
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('inputFile', type=str)
+	parser.add_argument('outputFile', type=str)
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	args = get_args()
+	input_file = args.inputFile
+	output_file = args.outputFile
+	with open(input_file, 'r') as in_xml:
+		filedict = xmltodict.parse(in_xml.read())
+		out_dict = json.dumps(filedict, indent=4, sort_keys=True)
+		with open(output_file, 'w') as out_json:
+			out_json.write(out_dict)
