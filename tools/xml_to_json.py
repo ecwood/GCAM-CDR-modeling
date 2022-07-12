553df921c9c85df775c65142fe4a324c9fef1033
Erica Wood <ericacwood@gmail.com>
2022-07-12 09:28:36 -0700

    #14 cleaned up xml_to_json

--- tools/xml_to_json.py
+++ tools/xml_to_json.py
@@ -12,8 +12,9 @@ if __name__ == '__main__':
 	args = get_args()
 	input_file = args.inputFile
 	output_file = args.outputFile
+	filedict = {}
 	with open(input_file, 'r') as in_xml:
 		filedict = xmltodict.parse(in_xml.read())
-		out_dict = json.dumps(filedict, indent=4, sort_keys=True)
-		with open(output_file, 'w') as out_json:
-			out_json.write(out_dict)
+	out_dict = json.dumps(filedict, indent=4, sort_keys=True)
+	with open(output_file, 'w') as out_json:
+		out_json.write(out_dict)
