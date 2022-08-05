0cfe608199a17b3274b44178f7e3b8bb40137017
Erica Wood <ericacwood@gmail.com>
2022-08-05 11:20:02 -0700

    #30 attempting to get solar/renewable data out

--- process_broken_debug_db.py
+++ process_broken_debug_db.py
@@ -5,13 +5,22 @@ import re
 
 SCENARIO_REGEX = '<scenario name="(.*)" date="(.*)">'
 REGION_REGEX = '<region name="(.*)" type="region">'
-SUPPLYSECTOR_REGEX = '<supplysector name="CO2 removal" type="sector">'
-SUBSECTOR_REGEX = '<subsector name="dac" type="subsector">'
-TECH_REGEX = '<technology year="(.*)" name="(.*)" type="technology">'
+SUPPLYSECTOR_DAC_REGEX = '<supplysector name="CO2 removal" type="sector">'
+SUPPLYSECTOR_SOLAR_REGEX = '<supplysector name="electricity" type="sector">'
+NESTING_SUBSECTOR_SOLAR_USA_REGEX = '<nesting-subsector name="solar" type="subsector">'
+SUBSECTOR_DAC_REGEX = '<subsector name="dac" type="subsector">'
+SUBSECTOR_SOLAR_REGEX = '<subsector name="(.*)" type="subsector">'
+SUBSECTOR_SOLAR_REGEX_USA = '<subsector depth="1" name="PV_base_storage" type="subsector">'
+TECH_DAC_REGEX = '<technology year="(.*)" name="(.*)" type="technology">'
+TECH_SOLAR_REGEX_GLOBAL = '<intermittent-technology year="(.*)" name="(.*)" type="technology">'
+TECH_SOLAR_REGEX_USA = '<technology year="(.*)" name="(.*)" type="technology">'
 COST_REGEX = '<cost unit="(.*)" year="(.*)">(.*)<\/cost>'
 CO2_REMOVAL_REGEX = '<CO2 name="CO2" type="GHG">'
-REMOVED_REGEX = '<emissions-sequestered unit="(.*)" year="(.*)">(.*)<\/emissions-sequestered>'
+SOLAR_OUTPUT_REGEX = '<output-primary name="(.*)" type="output">'
+REMOVED_DAC_REGEX = '<emissions-sequestered unit="(.*)" year="(.*)">(.*)<\/emissions-sequestered>'
+SOLAR_ELEC_REGEX = '<physical-output unit="(.*)" vintage="(.*)">(.*)<\/physical-output>'
 TECH_END_REGEX = '</technology>'
+INTER_TECH_END_REGEX = '</intermittent-technology>'
 SUBSECTOR_END_REGEX = '</subsector>'
 SUPPLYSECTOR_END_REGEX = '</supplysector>'
 REGION_END_REGEX = '</region>'
@@ -41,22 +50,45 @@ def check_region(line):
 		return {'region': mat_chk[1]}
 	return False
 
-def check_supplysector(line):
-	mat_chk = re.match(SUPPLYSECTOR_REGEX, line)
+def check_supplysector_dac(line):
+	mat_chk = re.match(SUPPLYSECTOR_DAC_REGEX, line)
 	if mat_chk:
 		return True
 	return False
 
-def check_subsector(line):
-	mat_chk = re.match(SUBSECTOR_REGEX, line)
+def check_supplysector_solar(line):
+	mat_chk_gl = re.match(SUPPLYSECTOR_SOLAR_REGEX, line)
+	mat_chk_usa = re.match(NESTING_SUBSECTOR_SOLAR_USA_REGEX, line)
+	if mat_chk_gl or mat_chk_usa:
+		return True
+	return False
+
+def check_subsector_dac(line):
+	mat_chk = re.match(SUBSECTOR_DAC_REGEX, line)
 	if mat_chk:
 		return True
 	return False
 
-def check_tech(line):
-	mat_chk = re.match(TECH_REGEX, line)
+def check_subsector_solar(line):
+	mat_chk_gl = re.match(SUBSECTOR_SOLAR_REGEX, line)
+	mat_chk_usa = re.match(SUBSECTOR_SOLAR_REGEX_USA, line)
+	if mat_chk_gl or mat_chk_usa:
+		return True
+	return False
+
+def check_tech_dac(line):
+	mat_chk = re.match(TECH_DAC_REGEX, line)
 	if mat_chk:
 		return {'year': mat_chk[1], 'name': mat_chk[2]}
+	return False
+
+def check_tech_solar(line):
+	mat_chk_gl = re.match(TECH_SOLAR_REGEX_GLOBAL, line)
+	mat_chk_usa = re.match(TECH_SOLAR_REGEX_USA, line)
+	if mat_chk_gl:
+		return {'year': mat_chk_gl[1], 'name': mat_chk_gl[2]}
+	if mat_chk_usa:
+		return {'year': mat_chk_usa[1], 'name': mat_chk_usa[2]}
 	return False
 
 def check_cost(line):
@@ -71,8 +103,20 @@ def check_co2_removal(line):
 		return True
 	return False
 
+def check_solar_output(line):
+	mat_chk = re.match(SOLAR_OUTPUT_REGEX, line)
+	if mat_chk:
+		return True
+	return False
+
 def check_removed(line):
-	mat_chk = re.match(REMOVED_REGEX, line)
+	mat_chk = re.match(REMOVED_DAC_REGEX, line)
+	if mat_chk:
+		return {'unit': mat_chk[1], 'year': mat_chk[2], 'value': mat_chk[3]}
+	return {}
+
+def check_solar_elec(line):
+	mat_chk = re.match(SOLAR_ELEC_REGEX, line)
 	if mat_chk:
 		return {'unit': mat_chk[1], 'year': mat_chk[2], 'value': mat_chk[3]}
 	return {}
@@ -95,8 +139,14 @@ def check_region_end(line):
 		return True
 	return False
 
-def check_tech_end(line):
+def check_tech_dac_end(line):
 	mat_chk = re.match(TECH_END_REGEX, line)
+	if mat_chk:
+		return True
+	return False
+
+def check_tech_solar_end(line):
+	mat_chk = re.match(INTER_TECH_END_REGEX, line)
 	if mat_chk:
 		return True
 	return False
@@ -104,27 +154,39 @@ def check_tech_end(line):
 def isdict(val):
 	return isinstance(val, dict)
 
+def is_state(name):
+	return len(name) == 2
+
 def process_file(filename):
 	file_dict = {}
 	SCENARIO = False
 	scenario_name = ""
 	scenario_date = ""
-	REGION = True
+	REGION = False
 	region_name = ""
-	SUPPLYSECTOR = False
-	SUBSECTOR = False
-	TECH = False
-	tech_year = ""
-	tech_name = ""
-	cost = ""
+	SUPPLYSECTOR_DAC = False
+	SUPPLYSECTOR_SOLAR = False
+	SUBSECTOR_DAC = False
+	SUBSECTOR_SOLAR = False
+	TECH_DAC = False
+	TECH_SOLAR = False
+	tech_dac_year = ""
+	tech_solar_year = ""
+	tech_dac_name = ""
+	tech_solar_name = ""
+	cost_dac = ""
+	cost_solar = ""
 	CO2_REMOVAL = False
+	SOLAR_OUTPUT = False
 	with open(filename, 'r') as file:
 		line_num = 0
 		for line in file:
 			line_num += 1
 			if line_num % 10000000 == 0:
 				print(line_num)
-				# print(SCENARIO, REGION, SUPPLYSECTOR, SUBSECTOR, TECH, CO2_REMOVAL)
+#			if is_state(region_name) and SUPPLYSECTOR_SOLAR and SUBSECTOR_SOLAR and TECH_SOLAR and SOLAR_OUTPUT:
+#				print(region_name)
+#				print(SCENARIO, REGION, SUPPLYSECTOR_SOLAR, SUBSECTOR_SOLAR, TECH_SOLAR, SOLAR_OUTPUT)
 			line = line.strip()
 			scen_val = check_scenario(line)
 			SCENARIO |= isdict(scen_val)
@@ -137,33 +199,58 @@ def process_file(filename):
 				if isdict(reg_val):
 					region_name = reg_val['region']
 			if REGION:
-				SUPPLYSECTOR |= check_supplysector(line)
-			if SUPPLYSECTOR:
-				SUBSECTOR |= check_subsector(line)
-			if SUBSECTOR:
-				tech_val = check_tech(line)
-				TECH |= isdict(tech_val)
-				if isdict(tech_val):
-					tech_year = tech_val['year']
-					tech_name = tech_val['name']
-			if TECH:
+				SUPPLYSECTOR_DAC |= check_supplysector_dac(line)
+				SUPPLYSECTOR_SOLAR |= check_supplysector_solar(line)
+			if SUPPLYSECTOR_DAC:
+				SUBSECTOR_DAC |= check_subsector_dac(line)
+			if SUPPLYSECTOR_SOLAR:
+				SUBSECTOR_SOLAR |= check_subsector_solar(line)
+			if SUBSECTOR_DAC:
+				tech_dac_val = check_tech_dac(line)
+				TECH_DAC |= isdict(tech_dac_val)
+				if isdict(tech_dac_val):
+					tech_dac_year = tech_dac_val['year']
+					tech_dac_name = tech_dac_val['name']
+			if SUBSECTOR_SOLAR:
+				tech_solar_val = check_tech_solar(line)
+				TECH_SOLAR |= isdict(tech_solar_val)
+				if isdict(tech_solar_val):
+					tech_solar_year = tech_solar_val['year']
+					tech_solar_name = tech_solar_val['name']
+			if TECH_DAC:
 				cost_val = check_cost(line)
 				if isdict(cost_val):
-					cost = cost_val['cost'] + ' ' + cost_val['unit']
+					cost_dac = cost_val['cost'] + ' ' + cost_val['unit']
 				CO2_REMOVAL |= check_co2_removal(line)
+			if TECH_SOLAR:
+				cost_val = check_cost(line)
+				if isdict(cost_val):
+					cost_solar = cost_val['cost'] + ' ' + cost_val['unit']
+				SOLAR_OUTPUT |= check_solar_output(line)
 			if CO2_REMOVAL:
 				removed_vals = check_removed(line)
 				# print(removed_vals, line)
-				if removed_vals.get('year', None) == tech_year:
-					file_dict.setdefault(scenario_name, {}).setdefault(region_name, {}).setdefault(tech_name, {})[tech_year] = {'cost': cost, 'co2 removed': removed_vals.get('value', '') + ' ' + removed_vals.get('unit', '')}
+				if removed_vals.get('year', None) == tech_dac_year:
+					file_dict.setdefault(scenario_name, {}).setdefault(region_name, {}).setdefault(tech_dac_name, {})[tech_dac_year] = {'cost': cost_dac, 'co2 removed': removed_vals.get('value', '') + ' ' + removed_vals.get('unit', '')}
 					CO2_REMOVAL = False
-					TECH = False
-			if check_tech_end(line):
-				TECH = False
+					TECH_DAC = False
+			if SOLAR_OUTPUT:
+				solar_elec_vals = check_solar_elec(line)
+				# print(removed_vals, line)
+				if solar_elec_vals.get('year', None) == tech_solar_year:
+					file_dict.setdefault(scenario_name, {}).setdefault(region_name, {}).setdefault(tech_solar_name, {})[tech_solar_year] = {'cost': cost_solar, 'energy': solar_elec_vals.get('value', '') + ' ' + solar_elec_vals.get('unit', '')}
+					SOLAR_OUTPUT = False
+					TECH_SOLAR = False
+			if check_tech_dac_end(line):
+				TECH_DAC = False
+			if check_tech_solar_end(line):
+				TECH_SOLAR = False
 			if check_subsector_end(line):
-				SUBSECTOR = False
+				SUBSECTOR_DAC = False
+				SUBSECTOR_SOLAR = False
 			if check_supplysector_end(line):
-				SUPPLYSECTOR = False
+				SUPPLYSECTOR_DAC = False
+				SUPPLYSECTOR_SOLAR = False
 			if check_region_end(line):
 				REGION = False
 	return file_dict
