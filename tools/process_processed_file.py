38d7977eb246b0d2b916d95dcbb92d125c3d52d0
Erica Wood <ericacwood@gmail.com>
2022-08-05 11:19:35 -0700

    #30

--- tools/process_processed_file.py
+++ tools/process_processed_file.py
@@ -19,18 +19,27 @@ def save_json(data, output_file):
 
 def get_sequestrated_levels(file_dict):
 	sequestration_dict = dict()
+	energy_dict = dict()
 	file_dict = file_dict['GCAM-USA_Reference']
 	for region in file_dict:
 		if len(region) > 2:
 			continue
-		for dac_type in file_dict[region]:
-			for year in file_dict[region][dac_type]:
-				if year not in sequestration_dict:
-					sequestration_dict[year] = 0
-				sequestration_dict[year] += float(file_dict[region][dac_type][year]['co2 removed'].split(' ')[0])
-	return sequestration_dict
+		for key in file_dict[region]:
+			if key in ['hightemp DAC NG', 'hightemp DAC elec', 'lowtemp DAC heatpump']:
+				for year in file_dict[region][key]:
+					if year not in sequestration_dict:
+						sequestration_dict[year] = 0
+					sequestration_dict[year] += float(file_dict[region][key][year]['co2 removed'].split(' ')[0])
+			else:
+				for year in file_dict[region][key]:
+					if key not in energy_dict:
+						energy_dict[key] = dict()
+					if year not in energy_dict[key]:
+						energy_dict[key][year] = 0
+					energy_dict[key][year] += float(file_dict[region][key][year]['energy'].split(' ')[0])
+	return sequestration_dict, energy_dict
 
 if __name__ == '__main__':
 	args = get_args()
-	sequestration_dict = get_sequestrated_levels(load_json(args.inputFile))
-	save_json(sequestration_dict, args.outputFile)
+	sequestration_dict, energy_dict = get_sequestrated_levels(load_json(args.inputFile))
+	save_json({"Sequestration": sequestration_dict, "Energy": energy_dict}, args.outputFile)
